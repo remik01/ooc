aac23b82919884d59ccef18b4ef8937aad90a844
remik <remik01@interia.pl>
2018-06-27 21:11:47 +0200

    test commit

--- sandbox.c
+++ sandbox.c
@@ -9,7 +9,7 @@ int main(int argc, char const *argv[])
 {
     Rectangle *myrec = malloc(sizeof(Rectangle));
 
-    set_width(myrec, 2);
+    set_width(myrec, 5);
     set_height(myrec, 11);
 
     printf("area: %d\n", get_area(myrec));
